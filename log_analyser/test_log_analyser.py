a4f3aec6518395b155371a8339dd7fc6e00df412
xkdgo <xkdgo@rambler.ru>
2018-09-15 12:51:01 +0500

    remarks of the teacher are corrected

--- log_analyser/test_log_analyser.py
+++ log_analyser/test_log_analyser.py
@@ -19,12 +19,17 @@ class LogAnalyzerTests(unittest.TestCase):
         Logfile = namedtuple('Logfile', 'path date ext')
         # test directory with test filenames
         # print(log_analyzer.catchfile("./test_log_files"))
-        self.assertEqual(log_analyzer.catchfile("./test_log_files"), Logfile(path='./test_log_files/nginx-access-ui.log-20180818.gz', date='20180818', ext='gz'))
+        self.assertEqual(log_analyzer.catchfile("./test_log_files"),
+                         Logfile(path='./test_log_files/nginx-access-ui.log-20180818.gz', date='20180818', ext='gz'))
 
     def test_nginx_log_parser(self):
-        line1 = "1.196.116.32 -  - [29/Jun/2017:03:50:22 +0300] \"GET /api/v2/banner/25019354 HTTP/1.1\" 200 927 \"-\" \"Lynx/2.8.8dev.9 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/2.10.5\" \"-\" \"1498697422-2190034393-4708-9752759\" \"dc7161be3\" 0.390"
+        line1 = "1.196.116.32 -  - [29/Jun/2017:03:50:22 +0300] \"" \
+                "GET /api/v2/banner/25019354 HTTP/1.1\"" \
+                " 200 927 \"-\" \"Lynx/2.8.8dev.9 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/2.10.5\"" \
+                " \"-\" \"1498697422-2190034393-4708-9752759\" \"dc7161be3\" 0.390"
         # print(log_analyzer.nginx_log_parser(line1))
-        self.assertEqual(log_analyzer.nginx_log_parser(line1), {'request_url': '/api/v2/banner/25019354', 'response_time': 0.39})
+        self.assertEqual(log_analyzer.nginx_log_parser(line1),
+                         {'request_url': '/api/v2/banner/25019354', 'response_time': 0.39})
 
 
 if __name__ == "__main__":
